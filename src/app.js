6b105ec437918589f4fee45ca443ccd3101ba5c5
ayushrajsingh0542 <aayushrajsingh11@gmail.com>
2025-07-25 19:24:41 +0530

    bug fix

--- src/app.js
+++ src/app.js
@@ -21,6 +21,11 @@ app.options("/profile/edit", cors(corsOptions)); //  handles preflight for all r
 
 // Middleware
 app.use(cookieParser());
+
+// ✅ Place webhook raw body parser BEFORE express.json
+app.post("/payment/webhook", express.raw({ type: "application/json" }));
+
+// Normal body parsing comes AFTER the webhook
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -29,57 +34,22 @@ const authRouter = require("./routes/authRoute.js");
 const profileRouter = require("./routes/profileRoute.js");
 const requestRouter = require("./routes/requestRoute.js");
 const userRouter = require("./routes/userRoute.js");
-const paymentRouter=require('./routes/paymentRoute')
+const paymentRouter=require('./routes/paymentRoute')   // ✅ This was missing from use()
 
 app.use(authRouter);
 app.use(profileRouter);
 app.use(requestRouter);
 app.use(userRouter);
-app.use(paymentRouter)
+app.use(paymentRouter);  // ✅ Ensure this line exists
 
 //  Start server after DB connects
 connectDB()
   .then(() => {
     console.log(" DB connected successfully");
     app.listen(3030, "0.0.0.0", () => {
-  console.log(" Server running on http://0.0.0.0:3030");
-});
+      console.log(" Server running on http://0.0.0.0:3030");
+    });
   })
   .catch((err) => {
     console.error(" DB connection error:", err);
   });
-
-// app.use("/user",(req,res,next)=>{
-//     //route handler
-//     console.log("handling user 1");
-//     //res.send("res 1")
-//     next()
-// },(req,res,next)=>{//express does not autmoatically goes to this req handler..so we use next
-//     console.log("handling user 2");
-//    // res.send("res 2")
-//    next()
-// }
-// )
-
-// app.use("/user",(req,res,next)=>{
-//     console.log("user 3");
-//     res.send("res 3")
-// })
-
-// app.use("/admin",auth);
-
-// app.get("/admin/Check",(req,res)=>{
-//     res.send("admin portal")
-// })
-
-// app.use((err,req,res,next)=>{
-//     if(err)
-//     res.status(err.status).send(err.message);
-// })
-
-
-
-
-
-
-
