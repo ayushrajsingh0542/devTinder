16e474f6ac5fdebe3a52ba1c6ea570013fa28506
ayushrajsingh0542 <aayushrajsingh11@gmail.com>
2025-07-26 14:45:08 +0530

    bug fix

--- src/app.js
+++ src/app.js
@@ -39,8 +39,7 @@ app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", requestRouter);
 app.use("/", userRouter);
-
-app.use("/", paymentRouter);
+app.use("/api", paymentRouter);
 
 //  Start server after DB connects
 connectDB()
